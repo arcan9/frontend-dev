ff3776e158524865d5186520d5de819573f3cfdc
Ashelle Cancio <vermillion707@gmail.com>
2023-01-30 21:57:13 -0800

    Created navigation and its router. #1

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <nav className='site-nav grid'>
+      <h1>React Folio</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
