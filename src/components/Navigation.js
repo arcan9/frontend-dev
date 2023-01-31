d22a50e748b93f6368081b67e8b268535b63097e
Ashelle Cancio <vermillion707@gmail.com>
2023-01-30 23:14:29 -0800

    Route: used Link component for nav links routing. #1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
     <nav className='site-nav grid'>
       <h1>React Folio</h1>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/'>About</Link>
+        </li>
+        <li>
+          <Link to='#projects'>Projects</Link>
+        </li>
+        <li>
+          <Link to='#contact'>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
