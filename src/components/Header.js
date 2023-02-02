b37f8b665fe90d50e88ad9ead5c906cbb62897cf
Ashelle Cancio <vermillion707@gmail.com>
2023-02-02 01:49:20 -0800

    Context: passed down to child components in Main and render About conditionally. #2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
 import React from "react";
 import image from "../images/undraw_dev_focus_re_6iwt.svg";
 
-export default function Header() {
-  return <img src={image} alt='header' />;
+export default function Header({ setAboutView }) {
+  return (
+    <>
+      <img src={image} alt='header' />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+        asperiores minus perspiciatis necessitatibus expedita ratione aliquam
+        placeat rerum velit earum?
+      </p>
+      <button type='button' onClick={() => setAboutView(true)}>
+        More Info
+      </button>
+    </>
+  );
 }
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Context from "../Context";
 
 // My components
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
+import About from "../components/About";
 
 export default function Main() {
+  const [aboutView, setAboutView] = useState(false);
+
   return (
-    <>
+    <Context.Provider value={aboutView}>
       <Navigation />
-      <Header />
-    </>
+      {aboutView ? <About /> : <Header setAboutView={setAboutView} />}
+    </Context.Provider>
   );
 }
