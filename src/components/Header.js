302e1dd1917dd5d8297defe07ff830586f7e79f5
Ashelle Cancio <vermillion707@gmail.com>
2023-02-01 22:25:39 -0800

    Components: header and about created. Image imported. #2

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import image from "../images/undraw_dev_focus_re_6iwt.svg";
+
+export default function Header() {
+  return <img src={image} alt='header' />;
+}
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,14 @@
 import React from "react";
+
+// My components
 import Navigation from "../components/Navigation";
+import Header from "../components/Header";
 
 export default function Main() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <Header />
+    </>
+  );
 }
