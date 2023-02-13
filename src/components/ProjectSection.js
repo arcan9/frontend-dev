510bd280e7dd260454f77560b2c3ea814d73c50b
Ashelle Cancio <vermillion707@gmail.com>
2023-02-12 23:32:55 -0800

    Added simple carousel to project section with controls. #9

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import nekkoh from "../images/nekkoh.png";
-import codejournal from "../images/codejournal.png";
+import { projects } from "./projects-data";
+// import nekkoh from "../images/nekkoh.png";
+// import codejournal from "../images/codejournal.png";
 
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectSection() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [next, setNext] = useState("next");
+
+  const handlePrev = () => {
+    setNext("prev");
+    // below calculates the previous of the data
+    // making sure to loop to through the array when direction buttons are clicked
+    setCurrentIndex((currentIndex + projects.length - 1) % projects.length);
+  };
+
+  const handleNext = () => {
+    setNext("next");
+    // below calculates the next of the data
+    // making sure to loop to through the array when direction buttons are clicked
+    setCurrentIndex((currentIndex + projects.length + 1) % projects.length);
+  };
+
   return (
     <section className='project-bg'>
       <h3 className='projects-header'>Projects</h3>
       <div className='project-grid grid'>
         <ProjectCard
-          title='NEKKOH'
-          summary='This is project Nekkoh'
-          image={nekkoh}
+          title={projects[currentIndex].title}
+          summary={projects[currentIndex].summary}
+          image={projects[currentIndex].image}
+          next={next}
         />
-        <Link to='/project-one'>
+        <Link to={projects[currentIndex].link}>
           <button type='button' className='project-btn button'>
             Detail
           </button>
         </Link>
-        <ProjectCard
-          title='CODE JOURNAL'
-          summary='This is project Code Journal'
-          image={codejournal}
-        />
-        <Link to='/project-two'>
-          <button type='button' className='project-btn button'>
-            Detail
+        <div className='controls'>
+          <button type='button' onClick={handlePrev}>
+            Prev
           </button>
-        </Link>
+          <button type='button' onClick={handleNext}>
+            Next
+          </button>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/projects-data.js
@@ -0,0 +1,19 @@
+import nekkoh from "../images/nekkoh.png";
+import codejournal from "../images/codejournal.png";
+
+export const projects = [
+  {
+    title: "NEKKOH",
+    summary: "This is project Nekkoh",
+    image: nekkoh,
+    link: "/project-one"
+  },
+  {
+    title: "CODE JOURNAL",
+    summary: "This is project Code Journal",
+    image: codejournal,
+    link: "/project-two"
+  }
+];
+
+export default projects;
