014563dfab1b7c8842522aa4cd3dbc8caf20084d
Ashelle Cancio <vermillion707@gmail.com>
2023-03-12 16:57:19 -0700

    Modifired fa-arrow.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -15,14 +15,14 @@ export default function ProjectCard({
       </a>
       <h3 className='project-title'>
         <a href={externalLink} target='_blank' rel='noopener noreferrer'>
-          <i className='fa-solid fa-arrow-up-right-from-square' /> {title}
+          {title} <i className='fa-solid fa-arrow-up-right-from-square' />
         </a>
       </h3>
       <p className='project-tagline'>{tagline}</p>
       <hr />
       <div>
         <a href={github} target='_blank' rel='noopener noreferrer'>
-          <i className='fa-solid fa-arrow-up-right-from-square' /> Github
+          Github <i className='fa-solid fa-arrow-up-right-from-square' />
         </a>
       </div>
     </div>
